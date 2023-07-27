e6ec732602379a33d38310e27f84c982e4af96ce
Duy Nguyen <duynguyen@Duys-MacBook-Pro-2.local>
2023-07-28 01:09:06 +0700

    refactor code mail module for multiple mail service

--- src/common/integrations/mail/transporters/mail.console.transporter.ts
+++ src/common/integrations/mail/transporters/mail.console.transporter.ts
@@ -2,8 +2,6 @@ import { IMail } from '../interfaces/mail.interface';
 import { IMailTransporter } from './mail.transporter';
 
 export class MailConsoleTransporter implements IMailTransporter<any> {
-    constructor() {}
-
     sendMail(mail: IMail) {
         console.log('mail console sent');
     }
